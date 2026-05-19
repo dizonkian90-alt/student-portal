8874dba289c35855855bf5476d9c991e7e925651
dizonkian90-alt <dizonkian90@gmail.com>
2026-05-19 10:21:55 +0800

    navigation drawer setup

---
+++ app/layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-app>
+        <v-navigation-drawer>
+
+        </v-navigation-drawer>
+
+        <v-app>
+
+        </v-app>
+
+        <v-main>
+
+        </v-main>
+    </v-app>
+</template>
+
+<script setup>
+
+</script>
